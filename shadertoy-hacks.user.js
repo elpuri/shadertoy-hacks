f600be98e9b4725a65d9a4fcec1911866b524c08
Juha Turunen <turunen@iki.fi>
2016-07-08 17:54:25 -0700

    Initial commit

---
+++ shadertoy-hacks.user.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name         Shadertoy-hacks
+// @namespace    http://juhaturunen.com
+// @version      0.1
+// @description  Small hacks to make shadertoy.com even more awesome
+// @author       Juha Turunen
+// @match        https://www.shadertoy.com/view/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+	
+	var altHandlers = {};
+	var ctrlHandlers = {};
+	altHandlers.ArrowDown = function() {
+		gShaderToy.resetTime();
+	};
+	
+	ctrlHandlers.d = function() {
+		gShaderToy.mCanvas.toBlob(function(imageBlob) {		
+			var l = document.createElement("a");
+			l.download = document.title + ".png";
+			l.href = URL.createObjectURL(imageBlob);
+			document.body.appendChild(l);
+			l.click();
+			// Firefox requires us to let the event loop spin once before getting rid of the link and the object URL
+			window.setTimeout(function() {
+				document.body.removeChild(l);
+				window.URL.revokeObjectURL(l.href);
+			}, 0);
+		});
+	};
+	
+    window.addEventListener("load", function() {
+        window.addEventListener("keydown", function(event) {
+            console.log(event.key);
+			if (event.altKey && event.key in altHandlers)
+				altHandlers[event.key]();
+			else if (event.ctrlKey && event.key in ctrlHandlers)
+				ctrlHandlers[event.key]();
+        });
+    });
+})();
